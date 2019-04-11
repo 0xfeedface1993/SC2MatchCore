b303a64900b7a076013d95540e68b8ddb11a6844
许庭淞 <virus1993@icloud.com>
2019-04-11 11:46:45 +0800

    Update source file

--- Tests/StarCraftMatchCoreTests/StarCraftMatchCoreTests.swift
+++ Tests/StarCraftMatchCoreTests/StarCraftMatchCoreTests.swift
@@ -13,6 +13,43 @@ final class StarCraftMatchCoreTests: XCTestCase {
         ("testExample", testExample), ("testTeamDB", testTeamDB)
     ]
     
+    func testZoneDB() {
+        configDatabase()
+        
+        deleteAllTeamRecords()
+        deleteAllZoneRecords()
+        deleteAllTeamZoneRecords()
+        
+        // 单个添加记录
+        save(zone: "美洲") { (isSuccess) in
+            XCTAssert(isSuccess, "*** 插入或更新赛区数据失败")
+        }
+        
+        // 批量添加记录
+        save(zones: ["美洲", "太平洋", "亚洲"]) { (isSuccess) in
+            XCTAssert(isSuccess, "*** 批量插入或更新赛区数据失败")
+        }
+        
+        let z = Zone()
+        try? z.findAll()
+        
+        let rows = z.rows()
+        // 更新数据
+        update(zone: rows[0].id, name: "大风州") { (isSuccess) in
+            XCTAssert(isSuccess, "*** 批量更新赛区数据失败")
+        }
+        
+        // 删除数据，使用id
+        remove(zoneID: rows[0].id) { (isSuccess) in
+            XCTAssert(isSuccess, "*** 删除有效赛区数据失败")
+        }
+        
+        // 删除数据，使用名称
+        remove(zoneName: "亚洲") { (isSuccess) in
+            XCTAssert(isSuccess, "*** 删除有效赛区数据失败")
+        }
+    }
+    
     func testTeamDB() {
         configDatabase()
         
@@ -20,8 +57,9 @@ final class StarCraftMatchCoreTests: XCTestCase {
         deleteAllZoneRecords()
         deleteAllTeamZoneRecords()
         
-        save(zone: "美洲") { (isSuccess) in
-            XCTAssert(isSuccess, "*** 插入或更新赛区数据失败")
+        let teams = [("熊猫战队", "A"), ("北京战队", "B"), ("昆明战队", "C"), ("斗鱼战队", "D")]
+        save(teamPack: teams) { (isSuccess) in
+            XCTAssert(isSuccess, "*** 批量插入或更新战队数据失败")
         }
     }
 }
